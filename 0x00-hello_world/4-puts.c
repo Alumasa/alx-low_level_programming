163414c57e1b15562eeb62ffc46237225e43ff6c
Alumasa <alumasa.v@gmail.com>
2022-04-07 12:15:50 -0700

    Print using puts, edit2

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,5 @@
 #include <stdio.h>
 
-
-
 /**
  *
  *  * main - Entry point
@@ -16,8 +14,7 @@ int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
-
-		return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 
 }
