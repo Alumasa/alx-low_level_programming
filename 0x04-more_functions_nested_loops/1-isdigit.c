972634e2913d5dbfa16f2344d03f310f596edfd9
Alumasa <alumasa.v@gmail.com>
2022-04-14 07:04:21 -0700

    Print numbers and multiplications

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - checks for a digit (0-9)
+ * @c: int to be checked
+ * Return: 1 if is a digit, 0 otherrwise
+ */
+
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9');
+}
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * mul - multiplies tiwo integers(a and b)
+ * @a: first integer to be multiplied
+ * @b: second integer to be multiplied
+ * Return: multiplication
+ */
+
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
